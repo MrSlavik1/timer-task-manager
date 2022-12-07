7824e44ca4c0fe4a389236dd009704319e1a6c39
MrSlavik1 <70208281+MrSlavik1@users.noreply.github.com>
2022-12-07 19:55:08 +0300

    Update to v1.1.3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Manager = require("./src/Manager");
 const IntervalManager = require("./src/IntervalManager");
-const latestCheck = require("./src/latest.js");
 
 module.exports = {
 	Manager,
